f93e1dd58a1790b3808ee94d85aec36c7e8c7214
Dylan-Kline <100173809+Dylan-Kline@users.noreply.github.com>
2022-02-23 03:30:09 -0600

    Create Grades.h

---
+++ C++Projects/Grading-Record/src/Grades.h
@@ -0,0 +1,18 @@
+#ifndef GRADES_H
+#define GRADES_H
+
+class Grades
+{
+
+
+public:
+
+	void getGrades(int*, int);
+	void sortGrades(int*, int);
+	double avgGrades(int*, int);
+	void printGrades(int*, int, double);
+
+
+
+};
+#endif

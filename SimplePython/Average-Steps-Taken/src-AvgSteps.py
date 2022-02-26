0bd1d1eac90b550045c72a28a25f36c50da4ff30
Dylan-Kline <100173809+Dylan-Kline@users.noreply.github.com>
2022-02-25 18:23:49 -0600

    Update src-AvgSteps.py

--- SimplePython/Average-Steps-Taken/src-AvgSteps.py
+++ SimplePython/Average-Steps-Taken/src-AvgSteps.py
@@ -12,23 +12,25 @@ Oct = 31
 Nov = 30
 Dec = 31
 
+
 #Define main
 def main():
     try:
         #Check if leap year
         year = input('Enter current year to be checked: ')
+        year = int(year)
 
         #If leap year, update Feb to 29 days
         if(year%4==0 and year%100!=0 or year%400==0):
             global Feb
             Feb = 29
-            return Feb
+            print(year, 'was a leap year')
         else:
             print('Not a leap year.')
-    
+        
         #Open file
         file = open('steps.txt', 'r')
-
+      
         #Average steps each month
         average_steps(file, 'January', Jan)
         average_steps(file, 'February', Feb)
@@ -42,7 +44,8 @@ def main():
         average_steps(file, 'October', Oct)
         average_steps(file, 'November', Nov)
         average_steps(file, 'December', Dec)
-
+        
+       
     #IOError
     except IOError as err:
         print('IOError:', err)
@@ -66,11 +69,16 @@ def main():
 #Define average steps
 def average_steps(file, month_name, days):
     sum = 0
+    
     for count in range(days):
-        sum += int(file.readline())
+        while file.readline() != '':
+            sum += int(file.readline())
+     
     average = sum / days
+    
     print('The average steps taken in', month_name, 'was',
           format(average, ',.1f'))
+
     
 #Call main function
 main()
